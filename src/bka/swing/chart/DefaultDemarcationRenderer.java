e8d3bcc4c1462239521ab33154260d6baf0d9d6b
Bart Kampers <github@bartkampers.nl>
2014-12-26 19:54:44 +0100

    Bug fix horizontal demarcations

--- src/bka/swing/chart/DefaultDemarcationRenderer.java
+++ src/bka/swing/chart/DefaultDemarcationRenderer.java
@@ -25,43 +25,59 @@ public class DefaultDemarcationRenderer extends DemarcationRenderer {
     }
     
   
+    @Override
     public void draw(Graphics2D g2d) {
         if (palette != null && palette.length > 0) {
-            DataSet dataSet = chartPanel.getDataSet();
             switch (chartPanel.getDemarcationMode()) {
-                case X: {
-                    int colorIndex = 0;
-                    java.util.List<Number> values = dataSet.xDemarcations.values;
-                    int count = values.size();
-                    for (int i = 1; i < count; i++) {
-                        int x0 = Math.max(dataSet.xPixel(values.get(i-1)), chartPanel.areaLeft());
-                        int x1 = Math.min(dataSet.xPixel(values.get(i)), chartPanel.areaRight());
-                        g2d.setColor(palette[colorIndex]);
-                        g2d.fillRect(x0, chartPanel.areaTop(), x1 - x0, chartPanel.areaHeight());
-                        colorIndex = (colorIndex + 1) % palette.length;
-                    }
+                case X:
+                    drawVerticalDemarcations(g2d);
                     break;
-                }
-                case Y: {
-                    int colorIndex = 0;
-                    int xMinPixel = dataSet.xPixel(dataSet.getXMin());
-                    int width = dataSet.xPixel(dataSet.getXMax()) - xMinPixel;
-                    java.util.List<Number> values = dataSet.yDemarcations.values;
-                    int count = values.size();
-                    for (int i = 1; i < count; i++) {
-                        int y0 = Math.min(dataSet.yPixel(values.get(i-1)), chartPanel.areaBottom());
-                        int y1 = Math.max(dataSet.yPixel(values.get(i)), chartPanel.areaTop());
-                        g2d.setColor(palette[colorIndex]);
-                        g2d.fillRect(xMinPixel, y0, width, y1 - y0);
-                        colorIndex = (colorIndex + 1) % palette.length;
-                    }
+                case Y:
+                    drawHorizontalDemarcations(g2d);
                     break;
-                }
             }
+        }
+    }
+
+    
+    private void drawVerticalDemarcations(Graphics2D g2d) {
+        int areaTop = chartPanel.areaTop();
+        int areaHeight = chartPanel.areaHeight();
+        int areaLeft = chartPanel.areaLeft();
+        int areaRight = chartPanel.areaRight();
+        DataSet dataSet = chartPanel.getDataSet();
+        java.util.List<Number> values = dataSet.xDemarcations.values;
+        int count = values.size();
+        int colorIndex = 0;
+        for (int i = 1; i < count; ++i) {
+            int left = Math.max(dataSet.xPixel(values.get(i-1)), areaLeft);
+            int right = Math.min(dataSet.xPixel(values.get(i)), areaRight);
+            g2d.setColor(palette[colorIndex]);
+            g2d.fillRect(left, areaTop, right - left, areaHeight);
+            colorIndex = (colorIndex + 1) % palette.length;
         }
     }
     
     
+    private void drawHorizontalDemarcations(Graphics2D g2d) {
+        int areaTop = chartPanel.areaTop();
+        int areaBottom = chartPanel.areaBottom();
+        int areaLeft = chartPanel.areaLeft();
+        int width = chartPanel.areaWidth();
+        DataSet dataSet = chartPanel.getDataSet();
+        java.util.List<Number> values = dataSet.yDemarcations.values;
+        int count = values.size();
+        int colorIndex = 0;
+        for (int i = 1; i < count; ++i) {
+            int bottom = Math.min(dataSet.yPixel(values.get(i-1)), areaBottom);
+            int top = Math.max(dataSet.yPixel(values.get(i)), areaTop);
+            g2d.setColor(palette[colorIndex]);
+            g2d.fillRect(areaLeft, top, width, bottom - top);
+            colorIndex = (colorIndex + 1) % palette.length;
+        }
+    }
+
+
     private void setDefaultPalette() {
         try {
             palette = (Color[]) javax.swing.UIManager.get("Chart.demarcationPalette");
